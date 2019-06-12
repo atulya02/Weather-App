40956ddd99b7d68d8b7f434a026a1b31d94cefdd
Atulya Raj <atulya.raj2904@gmail.com>
2019-06-12 15:01:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,87 +1,42 @@
-import React,{Component} from 'react';
-import Titles from './components/Titles';
-import Form from './components/Form';
-import Weather from './components/Weather';
-import { async } from 'q';
+import React, { Component } from 'react'
 
-const API_KEY="6c5b932e0e4cf33fe65818d5bc88a4e3";
-
-
-class App extends Component {
-
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-       temperature:undefined,
-       city:undefined,
-       country:undefined,
-       humidity:undefined,
-       description:undefined,
-       error:undefined
+export class Weather extends Component {
+    render() {
+        return (
+            <div className='weather__info'>
+                {this.props.city && this.props.country && <p className='weather__key'>Location:
+                <span className='weather__value'>
+                 {this.props.city},{this.props.country}
+                </span>
+                </p>}
+                <br/>
+                {this.props.temperature &&<p className='weather__key'>Temperature: 
+                <span className='weather__value'>
+                 {this.props.temperature}
+                </span>
+                </p>}
+                <br/>
+                {this.props.humidity && <p className='weather__key'>Humidity:
+                <span className='weather__value'>
+                 {this.props.humidity}
+                </span></p>}
+                <br/>
+                {this.props.description && <p className='weather__key'>Description:
+                <span className='weather__value'>
+                 {this.props.description}
+                </span>
+                </p>}
+                {this.props.icon && <p className='weather__key'>
+                <span className='weather__value'>
+                <img src={this.props.error}></img>
+                </span></p>}
+                {this.props.error && <p className='weather__key'>Error:
+                <span className='weather__value'>
+                {this.props.error}
+                </span></p>}
+            </div>
+        )
     }
-  }
-  
-  
-  
-  getWeather=async(e) => {
-    e.preventDefault(); 
-    const city=e.target.elements.City.value;
-    const country=e.target.elements.Country.value;
-    const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-    const data=await api_call.json();
-    
-if(city && country)
-   {console.log(data);
-     
-    this.setState({
-      temperature:data.main.temp,
-      city:data.name,
-      country:data.sys.country,
-      humidity:data.main.humidity,
-      description:data.weather[0].description,
-      error: ""
-    })
-  }else
-  {
-    this.setState({
-      error:'Enter City & Country'
-    })
-  }
-  }
-
-  render() {
-    return (
-      
-        <div>
-           <div className='wrapper'>
-             <div className='main'>
-               <div className='container'>
-                 <div className='row'>
-                   <div className='col-xs-5 title-container'><Titles/></div>
-                   <div className='col-xs-7 form-container'>
-                   <Form getWeather={this.getWeather}/>
-                    <Weather 
-                    temperature={this.state.temperature}
-                    city={this.state.city}
-                    country={this.state.country}
-                    humidity={this.state.humidity}
-                    description={this.state.description}
-                    error={this.state.error}/>
-                   </div>
-                 </div>
-               </div>
-             </div>
-           </div>
-        </div>
-         
-      
-    );
-  }
 }
 
-          
-
-export default App;
-
-
+export default Weather
